a267f97a3e168c032a21b6024cf475672648c1d0
Mr.Robot <128728937+SecurityLeader@users.noreply.github.com>
2023-04-04 10:09:32 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt install python -y
+apt install python2 -y
+apt install ruby -y
+gem install lolcat
+echo '
+ ==============================================
+|         Coded By Mr.Robot                    | 
+|         Now Type python3 IP-information.py   |
+ =============================================='
